26dfb42862924a825677822f4ab3f8599aca60d9
Thijme van Son <thijme@thirantos.com>
2025-06-10 21:10:15 +0200

    improved error message

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,23 @@ use tectonic;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
 
+fn as_detailed_message(err: &dyn Error) -> String {
+    let mut msg = err.to_string();
+    let mut current = err.source();
+    while let Some(cause) = current {
+        msg.push_str("\nCaused by: ");
+        msg.push_str(&cause.to_string());
+        current = cause.source();
+    }
+    msg
+}
+
+
 #[pyfunction]
 fn latex_to_pdf(py: Python, source: &str) -> PyResult<Py<PyBytes>> {
     match tectonic::latex_to_pdf(source) {
-        Ok(pdf_bytes) => {
-            Ok(PyBytes::new(py, &pdf_bytes).into())
-        },
-        Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(err.to_string())),
+        Ok(pdf_bytes) => Ok(PyBytes::new(py, &pdf_bytes).into()),
+        Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(as_detailed_message(&err)),
     }
 }
 
